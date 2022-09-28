2a68ee053246c8960b307bb3a0afeba5f29661a4
georgeunidev <104032344+georgeunidev@users.noreply.github.com>
2022-09-28 15:02:32 +0100

    Update ready.js

--- ready.js
+++ ready.js
@@ -1,9 +1,7 @@
 const client = require('../index')
-
 client.once('ready', async() => {
     console.log(`${client.user.tag} is ready!`)
     client.user.setActivity('~help', { type: 'STREAMING', url: 'https://www.twitch.tv/discord' })
-
     client.guilds.cache.forEach(async(guild) => {
         if (guild.available) {
             await guild.members.fetch().then((member) => {
